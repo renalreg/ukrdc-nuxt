88276b03bb3eae6425f9aca68b8e58c1935bf6d4
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-02 15:57:46 +0000

    Added basic workitem list with pagination

---
+++ components/paginator.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav
+    class="px-4 py-3 flex items-center justify-between sm:px-6"
+    aria-label="Pagination"
+  >
+    <div class="hidden sm:block">
+      <p class="text-sm text-gray-700">
+        Showing
+        <span class="font-medium">{{ page * size + 1 }}</span>
+        to
+        <span class="font-medium">{{ (page + 1) * size }}</span>
+        of
+        <span class="font-medium">{{ total }}</span>
+        results
+      </p>
+    </div>
+    <div class="flex-1 flex justify-between sm:justify-end">
+      <a
+        v-show="page > 0"
+        href="#"
+        class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        @click="$emit('prev')"
+      >
+        Previous
+      </a>
+      <a
+        href="#"
+        class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        @click="$emit('next')"
+      >
+        Next
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    page: {
+      type: Number,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style></style>
--- pages/workitems.vue
+++ pages/workitems.vue
@@ -3,6 +3,86 @@
     <div class="max-w-7xl mx-auto mb-4">
       <h1 class="text-2xl font-semibold text-gray-900">Work Items</h1>
     </div>
+
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div
+            class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+          >
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Description
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Records
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Updated
+                  </th>
+                  <th scope="col" class="relative px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <tr v-for="item in workitems" :key="item.id">
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                  >
+                    {{ item.id }}
+                  </td>
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 max-w-sm truncate"
+                  >
+                    {{ item.description }}
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {{ item.personId }} → {{ item.masterId }}
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {{ formatDate(item.lastUpdated) }}
+                  </td>
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                  >
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <paginator
+              class="bg-white border-t border-gray-200"
+              :page="page"
+              :size="size"
+              :total="total"
+              @next="changePage(page + 1)"
+              @prev="changePage(page - 1)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,12 +120,21 @@ export default Vue.extend({
   },
   head() {
     return {
-      title: 'Dashboard',
+      title: 'Work Items',
     }
   },
-  methods: {},
+  methods: {
+    changePage(newPage: number): void {
+      this.page = newPage
+      this.$fetch()
+    },
+    formatDate(rawDate: string): string {
+      const d: number = Date.parse(rawDate)
+      const newDate = new Date(d)
+      return newDate.toLocaleString()
+    },
+  },
 })
 </script>
 
-<style>
-</style>
+<style></style>
