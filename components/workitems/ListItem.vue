1bab0822cf8fafe121d285533deec1bf399f9936
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-03 14:05:34 +0000

    Separated list elements and added query pagination

---
+++ components/workitems/ListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <li>
+    <a href="#" class="block px-4 py-4 bg-white hover:bg-gray-50">
+      <span class="flex items-center space-x-4">
+        <span class="flex-1 flex space-x-2 truncate">
+          <span class="flex flex-col text-gray-500 text-sm truncate">
+            <span class="truncate">{{ item.id }} - {{ item.description }}</span>
+            <span class="text-gray-900 font-medium"
+              >{{ item.personId }} → {{ item.masterId }}</span
+            >
+
+            <span>{{ formatDate(item.lastUpdated) }}</span>
+          </span>
+        </span>
+        <!-- Heroicon name: solid/chevron-right -->
+        <svg
+          class="flex-shrink-0 h-5 w-5 text-gray-400"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </span>
+    </a>
+  </li>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import { WorkItemShort } from '@/interfaces/workitems'
+import dateUtilsMixin from '@/mixins/dateutils'
+
+export default Vue.extend({
+  mixins: [dateUtilsMixin],
+  props: {
+    item: {
+      type: Object as () => WorkItemShort,
+      required: true,
+    },
+  },
+})
+</script>
--- pages/workitems.vue
+++ pages/workitems.vue
@@ -9,47 +9,21 @@
       <div
         class="shadow overflow-hidden border-b border-gray-200 rounded-md sm:rounded-lg max-w-7xl"
       >
-        <!-- Activity list (smallest breakopoint only) -->
+        <!-- Activity list (up to large breakpoint) -->
         <div class="shadow lg:hidden">
           <ul class="divide-y divide-gray-200 overflow-hidden shadow">
-            <li v-for="item in workitems" :key="item.id">
-              <a href="#" class="block px-4 py-4 bg-white hover:bg-gray-50">
-                <span class="flex items-center space-x-4">
-                  <span class="flex-1 flex space-x-2 truncate">
-                    <span class="flex flex-col text-gray-500 text-sm truncate">
-                      <span class="truncate"
-                        >{{ item.id }} - {{ item.description }}</span
-                      >
-                      <span class="text-gray-900 font-medium"
-                        >{{ item.personId }} → {{ item.masterId }}</span
-                      >
-
-                      <span>{{ formatDate(item.lastUpdated) }}</span>
-                    </span>
-                  </span>
-                  <!-- Heroicon name: solid/chevron-right -->
-                  <svg
-                    class="flex-shrink-0 h-5 w-5 text-gray-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </li>
+            <workitemsListItem
+              v-for="item in workitems"
+              :key="item.id"
+              :item="item"
+            >
+            </workitemsListItem>
           </ul>
         </div>
 
-        <!-- Activity table (small breakopoint and up) -->
+        <!-- Activity table (large breakopoint and up) -->
         <table
-          class="hidden table-auto lg:table w-full divide-y divide-gray-200"
+          class="hidden lg:table table-auto w-full divide-y divide-gray-200"
         >
           <thead class="bg-gray-50">
             <tr>
@@ -83,25 +57,12 @@
             </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
-            <tr v-for="item in workitems" :key="item.id">
-              <td class="px-6 py-4 text-sm font-medium text-gray-900">
-                {{ item.id }}
-              </td>
-              <td class="px-6 py-4 text-sm text-gray-500">
-                {{ truncateString(item.description, 60) }}
-              </td>
-              <td class="px-2 py-4 text-sm text-gray-500">
-                {{ item.personId }} → {{ item.masterId }}
-              </td>
-              <td class="px-6 py-4 text-sm text-gray-500">
-                {{ formatDate(item.lastUpdated) }}
-              </td>
-              <td class="px-6 py-4 text-right text-sm font-medium max-w-min">
-                <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                  >Edit</a
-                >
-              </td>
-            </tr>
+            <workitemsTableItem
+              v-for="item in workitems"
+              :key="item.id"
+              :item="item"
+            >
+            </workitemsTableItem>
           </tbody>
         </table>
 
@@ -122,6 +83,7 @@
 import Vue from 'vue'
 
 import { WorkItemShort } from 'interfaces/workitems'
+import dateUtilsMixin from '@/mixins/dateutils'
 
 interface WorkItemPage {
   items: WorkItemShort[]
@@ -131,11 +93,12 @@ interface WorkItemPage {
 }
 
 export default Vue.extend({
+  mixins: [dateUtilsMixin],
   data() {
     return {
       workitems: [] as WorkItemShort[],
       total: 0,
-      page: 0,
+      page: (this.$route.query.page || 0) as Number,
       size: 20,
     }
   },
@@ -159,20 +122,10 @@ export default Vue.extend({
     changePage(newPage: number): void {
       this.page = newPage
       this.$fetch()
-    },
-    formatDate(rawDate: string): string {
-      const d: number = Date.parse(rawDate)
-      const newDate = new Date(d)
-      return newDate.toLocaleString()
-    },
-    truncateString(str: string, num: number): string {
-      // If the length of str is less than or equal to num
-      // just return str--don't truncate it.
-      if (str.length <= num) {
-        return str
-      }
-      // Return str truncated with '...' concatenated to the end of str.
-      return str.slice(0, num) + '...'
+      this.$router.push({
+        path: this.$route.path,
+        query: { page: newPage.toString() },
+      })
     },
   },
 })
