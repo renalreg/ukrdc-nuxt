f6a25ac6eb83edc8f0f81cc54381de579c17920d
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-04 15:27:04 +0000

    Added password reset modal to profile

---
+++ components/modal/Confirm.vue
@@ -0,0 +1,187 @@
+<template>
+  <transition :duration="200">
+    <div v-show="visible" class="fixed z-10 inset-0 overflow-y-auto">
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <!-- Background overlay, show/hide based on modal state. -->
+        <transition
+          enter-active-class="ease-out"
+          enter-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="ease-in"
+          leave-class="opacity-100"
+          leave-to-class="opacity-0 scale-90"
+        >
+          <div
+            v-if="visible"
+            class="fixed inset-0 transition-opacity"
+            aria-hidden="true"
+            @click="hide()"
+          >
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+        </transition>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+          >&#8203;</span
+        >
+        <!-- Modal panel, show/hide based on modal state. -->
+        <transition
+          enter-active-class="ease-out"
+          enter-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+          leave-active-class="ease-in"
+          leave-class="opacity-100 translate-y-0 sm:scale-100"
+          leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            v-show="visible"
+            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <div class="sm:flex sm:items-start">
+              <div
+                class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10"
+                :class="danger ? 'bg-red-100' : 'bg-indigo-100'"
+              >
+                <!-- Heroicon name: outline/exclamation -->
+                <svg
+                  class="h-6 w-6"
+                  :class="danger ? 'text-red-600' : 'text-indigo-600'"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    :d="modalIcon"
+                  />
+                </svg>
+              </div>
+              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  id="modal-headline"
+                  class="text-lg leading-6 font-medium text-gray-900"
+                >
+                  {{ title }}
+                </h3>
+                <div class="mt-2">
+                  <p class="text-sm text-gray-500">
+                    {{ message }}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                :class="
+                  danger
+                    ? ['bg-red-600', 'hover:bg-red-700', 'focus:ring-red-500']
+                    : [
+                        'bg-indigo-600',
+                        'hover:bg-indigo-700',
+                        'focus:ring-indigo-500',
+                      ]
+                "
+                @click="confirm()"
+              >
+                {{ confirmLabel }}
+              </button>
+              <button
+                type="button"
+                class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                @click="cancel()"
+              >
+                {{ cancelLabel }}
+              </button>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    confirmLabel: {
+      type: String,
+      required: false,
+      default: 'Confirm',
+    },
+    cancelLabel: {
+      type: String,
+      required: false,
+      default: 'Cancel',
+    },
+    danger: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  computed: {
+    modalIcon(): string {
+      if (this.icon !== null) {
+        return this.icon
+      } else if (this.danger) {
+        return 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
+      } else {
+        return 'M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+      }
+    },
+  },
+  methods: {
+    confirm(): void {
+      this.$emit('confirm')
+      this.hide()
+    },
+    cancel(): void {
+      this.$emit('cancel')
+      this.hide()
+    },
+    show(): void {
+      this.visible = true
+    },
+    hide(): void {
+      this.visible = false
+    },
+    toggle(): void {
+      this.visible = !this.visible
+    },
+  },
+})
+</script>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+    <ModalConfirm
+      ref="resetPasswordAlert"
+      title="Reset your password"
+      message="Are you sure you want to reset your password? You will receieve an email shortly to start the reset."
+      icon="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+      @confirm="resetPasswordAuth0()"
+    />
+    <div class="max-w-4xl mx-auto px-4 sm:px-6 md:px-8">
       <div
         v-if="$auth.loggedIn"
-        class="max-w-3xl md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl"
+        class="max-w-3xl lg:flex lg:items-center lg:justify-between lg:space-x-5 lg:max-w-7xl"
       >
         <div class="flex items-center space-x-5">
           <div class="flex-shrink-0">
@@ -30,11 +37,18 @@
           </div>
         </div>
         <div
-          class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3"
+          class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 lg:mt-0 lg:flex-row lg:space-x-3"
         >
           <button
             type="button"
-            class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+            class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            @click="showResetPasswordAlert()"
+          >
+            Reset Password
+          </button>
+          <button
+            type="button"
+            class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
             @click="$auth.logout()"
           >
             Sign out
@@ -48,11 +62,43 @@
 <script lang="ts">
 import Vue from 'vue'
 
+interface ResetPasswordPayload {
+  // eslint-disable-next-line camelcase
+  client_id: string
+  email: string
+  connection: string
+}
+
 export default Vue.extend({
+  data() {
+    return {
+      auth: this.$auth,
+    }
+  },
   head() {
     return {
       title: 'Profile',
     }
+  },
+
+  methods: {
+    showResetPasswordAlert(): void {
+      this.$refs.resetPasswordAlert.show()
+    },
+    resetPasswordAuth0(): void {
+      const domain: string = this.$auth.strategies.auth0.options.domain
+      const clientId: string = this.$auth.strategies.auth0.options.clientId
+      const userEmail: string = this.$auth.user.email
+      const url: string = `https://${domain}/dbconnections/change_password`
+      const payload: ResetPasswordPayload = {
+        client_id: clientId,
+        email: userEmail,
+        connection: 'Username-Password-Authentication',
+      }
+      this.$axios.$post(url, payload).then((res: string) => {
+        console.log(res)
+      })
+    },
   },
 })
 </script>
