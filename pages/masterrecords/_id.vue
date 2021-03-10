482a4073357a3060a39c0d2fdd79ae62583ed3c6
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-10 18:31:36 +0000

    Removed time from DoB

--- pages/masterrecords/_id.vue
+++ pages/masterrecords/_id.vue
@@ -57,7 +57,7 @@
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Date of Birth</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                {{ formatDate(record.dateOfBirth) }}
+                {{ formatDate(record.dateOfBirth, (t = false)) }}
               </dd>
             </div>
             <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
