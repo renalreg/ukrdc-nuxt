6fa2b64cc5c38854cf9f35f9851565886f8d5c20
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-10 17:34:31 +0000

    Consistent card rounding

--- pages/masterrecords/_id.vue
+++ pages/masterrecords/_id.vue
@@ -29,7 +29,7 @@
         </div>
       </div>
 
-      <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="bg-white shadow overflow-hidden rounded-md">
         <div class="px-4 py-5 sm:px-6">
           <h3 class="text-lg leading-6 font-medium text-gray-900 capitalize">
             {{ record.givenname.toLowerCase() }}
