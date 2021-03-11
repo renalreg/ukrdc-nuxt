74ce4660895bcabc7aa70e167392f3090877aa65
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-11 14:11:37 +0000

    Added NHS gender code lookup utility

---
+++ mixins/coddeutils.ts
@@ -0,0 +1,16 @@
+export default {
+  methods: {
+    formatGender(genderCode: number | string): string {
+      // https://datadictionary.nhs.uk/attributes/person_gender_code.html
+      if (genderCode === 1 || genderCode === '1') {
+        return 'Male'
+      } else if (genderCode === 2 || genderCode === '2') {
+        return 'Female'
+      } else if (genderCode === 9 || genderCode === '9') {
+        return 'Not Specified'
+      } else {
+        return 'Not Known'
+      }
+    },
+  },
+}
