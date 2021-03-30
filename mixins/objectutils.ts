e44c3ad150fc17c7556b5d46e49acfef0f7c2f93
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-30 16:44:37 +0100

    Handle null or undefined objects

--- mixins/objectutils.ts
+++ mixins/objectutils.ts
@@ -1,6 +1,9 @@
 export default {
   methods: {
     isEmptyObject(someObject: Object) {
+      if (someObject === null || someObject === undefined) {
+        return true
+      }
       return Object.keys(someObject).length === 0
     },
   },
