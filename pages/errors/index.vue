0aa07ddf462f1e1a891c716eba9d064d5ff49178
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-31 17:30:59 +0100

    Added date range to error list

--- pages/errors/index.vue
+++ pages/errors/index.vue
@@ -4,6 +4,16 @@
       <h1 class="text-2xl font-semibold text-gray-900">Errors</h1>
     </div>
 
+    <div class="mb-4">
+      <TDatePicker
+        v-model="dateRange"
+        :range="true"
+        :close-on-select="false"
+        :clearable="false"
+        :max-date="today"
+      />
+    </div>
+
     <div class="bg-white shadow overflow-hidden rounded-md">
       <!-- Skeleton results -->
       <ul v-if="$fetchState.pending" class="divide-y divide-gray-200">
@@ -28,6 +38,8 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import TDatePicker from 'vue-tailwind/dist/t-datepicker'
+import { singleQuery } from '@/utilities/queryUtils'
 
 import dateUtilsMixin from '@/mixins/dateutils'
 import { Message } from '@/interfaces/errors'
@@ -39,21 +51,43 @@ interface MessagePage {
   size: number
 }
 
+function todayString(addDays: number = 0): string {
+  const today = new Date()
+  today.setDate(today.getDate() + addDays)
+  return (
+    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+  )
+}
+
 export default Vue.extend({
+  components: { TDatePicker },
   mixins: [dateUtilsMixin],
   data() {
     return {
       messages: [] as Message[],
       total: 0,
-      page: (this.$route.query.page || 0) as Number,
+      page: (singleQuery(this.$route.query.page) || 0) as number,
       size: 20,
+      since: (singleQuery(this.$route.query.since) ||
+        todayString(-7)) as string,
+      until: (singleQuery(this.$route.query.until) || todayString(0)) as string,
     }
   },
   async fetch() {
     // Fetch the dashboard response from our API server
-    const res: MessagePage = await this.$axios.$get(
-      `/api/errors?status=ERROR&page=${this.page}&size=${this.size}`
-    )
+    let path = `/api/errors?status=ERROR&page=${this.page}&size=${this.size}`
+    if (this.since) {
+      path = path + `&since=${this.since}`
+    }
+    // Pass `until` to API if it's given
+    if (this.until) {
+      path = path + `&until=${this.until}`
+    } else if (this.since) {
+      // If no `until` is given but a `since` is given, then a single date is selected
+      // In this case we want to only show that one day, not a range
+      path = path + `&until=${this.since}`
+    }
+    const res: MessagePage = await this.$axios.$get(path)
     this.messages = res.items
     this.total = res.total
     this.page = res.page
@@ -63,6 +97,29 @@ export default Vue.extend({
     return {
       title: 'Mirth Errors',
     }
+  },
+  computed: {
+    today(): string {
+      return todayString(0)
+    },
+    dateRange: {
+      get(): string[] {
+        return [this.since, this.until]
+      },
+      set(newRange: string[]) {
+        this.since = newRange[0]
+        this.until = newRange[1]
+
+        const newQuery = Object.assign({}, this.$route.query, {
+          since: this.since,
+          until: this.until,
+        })
+        this.$router.push({
+          path: this.$route.path,
+          query: newQuery,
+        })
+      },
+    },
   },
   watch: {
     // We want to re-trigger the query if the route query changes,
---
+++ utilities/queryUtils.ts
@@ -0,0 +1,11 @@
+export function singleQuery(query: string | number | (string | number | null)[]): string | number | null {
+  // Return a single value form a query string
+  // If multiple values are passed as an array,
+  // only the zeroth value is returned
+  if (Array.isArray(query)) {
+    return query[0]
+  }
+  else {
+    return query
+  }
+}
