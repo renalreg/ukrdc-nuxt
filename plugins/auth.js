b3c8844c94579f235715ce276d400ff2ea76a540
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-07 18:58:01 +0100

    Fix production config

--- plugins/auth.js
+++ plugins/auth.js
@@ -3,8 +3,11 @@ const urljoin = require('url-join')
 export default ({ $auth, $config }) => {
   // See https://github.com/nuxt-community/auth-module/issues/1070
   $auth.onRedirect((to, _) => {
+    const basePath = ($config.app || $config._app).basePath
+    console.log($config)
+    console.log(basePath)
     if (!process.server) return
-    if (to.startsWith(decodeURI($config.app.basePath))) return to
-    return urljoin($config.app.basePath, to)
+    if (to.startsWith(decodeURI(basePath))) return to
+    return urljoin(basePath, to)
   })
 }
