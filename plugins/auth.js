b8368f3613e48f2808a9f2dee1f6f7c7ff3685d7
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-08 17:05:01 +0100

    Pull auth base if required

--- plugins/auth.js
+++ plugins/auth.js
@@ -3,9 +3,7 @@ const urljoin = require('url-join')
 export default ({ $auth, $config }) => {
   // See https://github.com/nuxt-community/auth-module/issues/1070
   $auth.onRedirect((to, _) => {
-    const basePath = ($config.app || $config._app).basePath
-    console.log($config)
-    console.log(basePath)
+    const basePath = ($config.app || $config._app).basePath || $auth.ctx.base
     if (!process.server) return
     if (to.startsWith(decodeURI(basePath))) return to
     return urljoin(basePath, to)
