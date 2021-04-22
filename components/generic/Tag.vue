d563b7a7b7ce5ad4bdd1c2412b828f0b19a3ea29
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-22 16:46:54 +0100

    Added generic tag component

---
+++ components/generic/Tag.vue
@@ -0,0 +1,28 @@
+<template>
+  <span
+    class="inline-flex items-center h-5 px-2.5 py-0.5 rounded-full text-xs font-medium"
+    :class="TagClass(nationalidType)"
+  >
+    <slot></slot>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    colour: {
+      type: String,
+      required: false,
+      default: 'gray',
+    },
+  },
+  methods: {
+    TagClass(): string[] {
+      return [`bg-${this.colour}-100`, `text-${this.colour}-800`]
+    },
+  },
+})
+</script>
+
+<style></style>
