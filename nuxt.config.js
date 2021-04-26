8879bcd34c51075d6d4c671431958a13c2a3b449
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-26 17:33:32 +0100

    Renamed permissions key variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,9 +95,8 @@ export default {
   publicRuntimeConfig: {
     // API root, used to construct API URLs within components
     apiBase: process.env.API_BASE || '/api',
-    // Access token key containing an array of permission group strings
-    accessTokenPermissionsKey:
-      process.env.ACCESS_TOKEN_PERMISSIONS_KEY || 'ukrdc',
+    // Nuxt-Auth user key containing an array of permission group strings
+    userPermissionsKey: process.env.USER_PERMISSIONS_KEY || 'ukrdc',
     // Axios public runtime config
     axios: {
       browserBaseURL: process.env.BROWSER_BASE_URL,
