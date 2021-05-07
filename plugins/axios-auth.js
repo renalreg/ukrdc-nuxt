71587b7d2bb2edec922c52c71e76af897b12fe45
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-07 13:54:55 +0100

    Fixed autoLogout check

--- plugins/axios-auth.js
+++ plugins/axios-auth.js
@@ -5,10 +5,12 @@ Automatically handle expired auth on Axios requests
 export default function ({ $axios, $auth }) {
   $axios.onRequest(() => {
     const { tokenExpired, refreshTokenExpired } = $auth.check(true)
-
     // Force reset if refresh token has expired
     // Or if `autoLogout` is enabled and token has expired
-    if (refreshTokenExpired || (tokenExpired && $auth.options.autoLogout)) {
+    if (
+      refreshTokenExpired ||
+      (tokenExpired && $auth.strategy.options.autoLogout)
+    ) {
       console.log('Auth token expired. Refreshing...')
       $auth.redirect('login')
     }
