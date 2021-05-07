733794d49309ae575422917ba13748df2d909ed0
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-07 12:04:39 +0100

    Added messageViewerModalInterface

--- interfaces/modal.ts
+++ interfaces/modal.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import { ConnectorMessage } from '@/interfaces/mirth'
 
 export interface modalInterface extends Vue {
   show: () => void
+  hide: () => void
+  toggle: () => void
+}
+
+export interface messageViewerModalInterface extends Vue {
+  show: (message: ConnectorMessage) => void
   hide: () => void
   toggle: () => void
 }
