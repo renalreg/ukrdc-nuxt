592bbec35060263a6f68968eb37d83e875c579bc
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-10 17:40:35 +0100

    Added tertiary button

---
+++ components/generic/button/Tertiary.vue
@@ -0,0 +1,22 @@
+<template>
+  <genericButtonBase
+    type="button"
+    class="border border-gray-300 shadow-sm"
+    :class="`focus:ring-${colour}-500`"
+    @click="$emit('click')"
+  >
+    <slot />
+  </genericButtonBase>
+</template>
+
+<script>
+export default {
+  props: {
+    colour: {
+      type: String,
+      required: false,
+      default: 'indigo',
+    },
+  },
+}
+</script>
