5ef5250c277fccf247b86132d67c832011b0a16e
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-11 14:23:34 +0100

    Switched to use useQuery

--- mixins/usePagination.ts
+++ mixins/usePagination.ts
@@ -1,48 +1,12 @@
-import {
-  ref,
-  useRoute,
-  useRouter,
-  watch,
-  onMounted,
-  computed,
-} from '@nuxtjs/composition-api'
-import { integerQuery } from '@/utilities/queryUtils'
+import { ref } from '@nuxtjs/composition-api'
+import useQuery from '@/mixins/useQuery'
 
 export default function () {
-  const route = useRoute()
-  const router = useRouter()
+  const { integerQuery } = useQuery()
 
-  const page = ref((integerQuery(route.value.query.page) || 0) as number)
+  const page = integerQuery('page', 0)
   const total = ref(0)
   const size = ref(20)
-
-  const pageQuery = computed(() => integerQuery(route.value.query.page) || 0)
-
-  watch(page, () => {
-    updateQueryFromPage()
-  })
-
-  watch(pageQuery, () => {
-    updatePageFromQuery()
-  })
-
-  onMounted(() => {
-    updateQueryFromPage()
-  })
-
-  function updateQueryFromPage() {
-    const newQuery = Object.assign({}, route.value.query, {
-      page: page.value.toString(),
-    })
-    router.push({
-      path: route.value.path,
-      query: newQuery,
-    })
-  }
-
-  function updatePageFromQuery() {
-    page.value = pageQuery.value as number
-  }
 
   function changePage(newPage: number): void {
     console.warn('changePage is deprecated. Directly set page value instead')
