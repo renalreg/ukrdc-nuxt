f8a03da4dffc321617dde288298c00139cccd828
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-17 17:10:52 +0100

    Fixed incorrectly named patientrecordsMedicationCard

--- pages/patientrecords/_pid/medications.vue
+++ pages/patientrecords/_pid/medications.vue
@@ -37,7 +37,7 @@
           :key="index"
           class="col-span-1"
         >
-          <medicationsCard :item="item" />
+          <patientrecordsMedicationCard :item="item" />
         </li>
       </ul>
     </div>
