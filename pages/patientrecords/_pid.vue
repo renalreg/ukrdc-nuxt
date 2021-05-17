bfcc13915472578398f798fc46d8e807d5c28403
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-17 17:11:11 +0100

    Added related record switcher

--- pages/patientrecords/_pid.vue
+++ pages/patientrecords/_pid.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <div v-if="record.patient" class="mb-2">
-      <TextH1>
-        {{ record.patient.names[0].given }} {{ record.patient.names[0].family }}
-      </TextH1>
-      <TextL1>
-        From {{ record.sendingfacility }} via {{ record.sendingextract }}
-      </TextL1>
+    <div v-if="record && record.patient" class="flex items-center mb-2">
+      <div class="flex-grow">
+        <TextH1>
+          {{ record.patient.names[0].given }}
+          {{ record.patient.names[0].family }}
+        </TextH1>
+        <TextL1>
+          {{ record.pid }}
+        </TextL1>
+      </div>
+      <div v-if="related">
+        <GenericSelect v-model="selectedPid">
+          <option
+            v-for="(item, index) in related"
+            :key="index"
+            :value="item.pid"
+          >
+            From {{ item.sendingfacility }} via {{ item.sendingextract }}
+          </option>
+        </GenericSelect>
+      </div>
     </div>
 
     <div class="mb-6"><GenericNavigationTabsLine :tabs="tabs" /></div>
 
-    <NuxtChild :record="record" />
+    <NuxtChild v-if="record" :record="record" />
   </div>
 </template>
 
@@ -23,17 +37,27 @@ import {
   useRoute,
   useContext,
   computed,
+  watch,
+  useRouter,
 } from '@nuxtjs/composition-api'
 
-import { PatientRecord } from '@/interfaces/patientrecord'
+import { PatientRecord, PatientRecordShort } from '@/interfaces/patientrecord'
 import { TabItem } from '@/interfaces/tabs'
 
 export default defineComponent({
   setup() {
     const route = useRoute()
+    const router = useRouter()
     const { $axios, $config, $hasPermission } = useContext()
 
-    const record = ref({} as PatientRecord)
+    const record = ref<PatientRecord>()
+    const related = ref<PatientRecordShort[]>()
+
+    const selectedPid = ref(route.value.params.pid)
+    watch(selectedPid, (value: string) => {
+      router.push({ name: route.value.name!, params: { pid: value } })
+    })
+
     const baseTabs = [
       {
         name: 'Overview',
@@ -72,12 +96,17 @@ export default defineComponent({
 
     useFetch(async () => {
       const path = `${$config.apiBase}/patientrecords/${route.value.params.pid}/`
+
       const res: PatientRecord = await $axios.$get(path)
       record.value = res
+      const rel: PatientRecordShort[] = await $axios.$get(`${path}related`)
+      related.value = rel
     })
 
     return {
       record,
+      related,
+      selectedPid,
       tabs,
     }
   },
