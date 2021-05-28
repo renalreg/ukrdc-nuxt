b9f77e1b0096968f53681fcbe28530c693874aa8
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-28 11:52:23 +0100

    Moved facility overview into dashboard

--- components/navigation/Sidebar.vue
+++ components/navigation/Sidebar.vue
@@ -119,12 +119,6 @@ export default defineComponent({
         visible: true,
       },
       {
-        title: 'Facilities',
-        url: '/facilities',
-        svg: 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4',
-        visible: true,
-      },
-      {
         title: 'Records',
         url: '/masterrecords',
         svg: 'M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4',
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <div class="max-w-7xl mx-auto mb-4">
-      <h1 class="text-2xl font-semibold text-gray-900">New Today</h1>
-    </div>
-
     <div class="mb-8">
       <genericAlertWarning v-for="message in response.warnings" :key="message" :message="message">
       </genericAlertWarning>
@@ -11,20 +7,41 @@
       <genericAlertInfo v-for="message in response.messages" :key="message" :message="message"> </genericAlertInfo>
     </div>
 
-    <div class="max-w-7xl mx-auto mb-8">
+    <!-- Admins will get workitems and records stats -->
+    <div v-if="response.workitems && response.ukrdcrecords" class="max-w-7xl mx-auto mb-8">
       <dashStats :workitems="response.workitems" :ukrdcrecords="response.ukrdcrecords" />
+    </div>
+
+    <div v-if="facilities">
+      <div v-if="facilities.length > 1">
+        <TextH2>My Facilities</TextH2>
+        <GenericSearchableSelect
+          v-model="selectedFacility"
+          class="my-4"
+          :options="facilityIds"
+          :labels="facilityLabels"
+          hint="Select a facility..."
+          :mount-opened="true"
+          :closable="false"
+        />
+      </div>
+
+      <FacilitiesOverview v-else-if="facilities.length > 0" :code="facilities[0].id" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, useFetch, useContext } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useFetch, useContext, watch, useRouter } from '@nuxtjs/composition-api'
+import useFacilities from '@/mixins/useFacilities'
 
 import { DashResponse } from '@/interfaces/dash'
 
 export default defineComponent({
   setup() {
+    const router = useRouter()
     const { $axios, $config } = useContext()
+    const { facilities, facilityIds, facilityLabels, selectedFacility, fetchFacilities } = useFacilities()
 
     const response = ref({} as DashResponse)
     const messages = ref([] as string[])
@@ -38,9 +55,21 @@ export default defineComponent({
       response.value = dashResponse
       messages.value = dashResponse.messages
       warnings.value = dashResponse.warnings
+
+      await fetchFacilities()
+    })
+
+    watch(selectedFacility, () => {
+      if (selectedFacility.value) {
+        router.push({ path: `/facilities/${selectedFacility.value}` })
+      }
     })
 
     return {
+      facilities,
+      facilityIds,
+      facilityLabels,
+      selectedFacility,
       response,
       messages,
       warnings,
