20b932a491a5cf1aa359721e8aac98feeb80944b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-06-17 17:34:24 +0100

    Include collapsible PatientRecord details

---
+++ components/patientrecords/DetailCards.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div v-if="!isEmptyObject(record) && record.patient.names && record.patient.names.length > 0" class="mt-4">
+      <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Names</h2>
+
+      <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="item in record.patient.names" :key="item.given + item.family" class="col-span-1">
+          <GenericCardMini class="px-4 py-2 w-full">
+            <TextL1> {{ item.given }} {{ item.family }} </TextL1>
+          </GenericCardMini>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="!isEmptyObject(record)" class="mt-4">
+      <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Patient Numbers</h2>
+      <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li
+          v-for="item in record.patient.numbers"
+          :key="item.numbertype + item.organization + item.patientid"
+          class="col-span-1 flex"
+        >
+          <GenericCardMini class="flex w-full">
+            <div
+              class="
+                flex-shrink-0 flex
+                items-center
+                justify-center
+                w-16
+                bg-indigo-600
+                text-white text-sm
+                font-medium
+                rounded-l-md
+              "
+            >
+              {{ item.numbertype }}
+            </div>
+            <div class="flex-1 flex items-center justify-between truncate">
+              <div class="flex-1 px-4 py-2 text-sm truncate">
+                <p class="text-gray-900 font-medium hover:text-gray-600 truncate">
+                  {{ item.patientid }}
+                </p>
+                <p class="text-gray-500">{{ item.organization }}</p>
+              </div>
+            </div>
+          </GenericCardMini>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="!isEmptyObject(record) && record.patient.addresses && record.patient.addresses.length > 0" class="mt-4">
+      <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Addresses</h2>
+
+      <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="item in record.patient.addresses" :key="item.street" class="col-span-1">
+          <GenericCardMini class="px-4 py-2 w-full">
+            <p class="text-gray-900 font-medium hover:text-gray-600">
+              {{ item.street }}
+            </p>
+            <p v-if="item.town" class="text-gray-500">{{ item.town }}</p>
+            <p v-if="item.county" class="text-gray-500">
+              {{ item.county }}
+            </p>
+            <p v-if="item.postcode" class="text-gray-500">
+              {{ item.postcode }}
+            </p>
+            <p v-if="item.countryDescription" class="text-gray-500">
+              {{ item.countryDescription }}
+            </p>
+            <p v-if="item.fromTime" class="text-gray-500">Since {{ formatDate(item.fromTime, (t = false)) }}</p>
+            <span
+              v-if="!item.toTime"
+              class="
+                flex-shrink-0
+                inline-block
+                px-2
+                py-0.5
+                text-green-800 text-xs
+                font-medium
+                bg-green-100
+                rounded-sm
+                mt-2
+              "
+              >Active</span
+            >
+            <span
+              v-else
+              class="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-sm mt-2"
+              >Inactive since {{ formatDate(item.toTime, (t = false)) }}</span
+            >
+          </GenericCardMini>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+import { formatDate } from '@/utilities/dateUtils'
+import { formatGender } from '@/utilities/codeUtils'
+import { isEmptyObject } from '@/utilities/objectUtils'
+
+import { PatientRecordShort } from '@/interfaces/patientrecord'
+
+export default defineComponent({
+  props: {
+    record: {
+      type: Object as () => PatientRecordShort,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+      formatDate,
+      formatGender,
+      isEmptyObject,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
--- components/patientrecords/ListItem.vue
+++ components/patientrecords/ListItem.vue
@@ -1,49 +1,51 @@
 <template>
   <li>
-    <NuxtLink :to="`/patientrecords/${item.pid}`" class="block hover:bg-gray-50">
-      <div class="flex items-center px-4 py-4 sm:px-6">
-        <div class="min-w-0 flex-1 flex items-center">
-          <div class="min-w-0 grid grid-cols-3 lg:grid-cols-4 md:gap-4 w-full">
-            <!-- Name, DoB, gender -->
-            <div>
-              <TextL1c class="capitalize truncate">
-                {{ item.sendingfacility }}
-              </TextL1c>
-              <TextP class="mt-2"> via {{ item.sendingextract }} </TextP>
-            </div>
-            <!-- National ID -->
-            <div class="text-right sm:text-left">
-              <TextL1>Local ID</TextL1>
-              <TextP class="mt-2">
-                {{ item.localpatientid }}
-              </TextP>
-            </div>
-            <!-- UKRDC ID -->
-            <div class="text-right sm:text-left">
-              <TextL1>UKRDC ID</TextL1>
-              <TextP class="mt-2">
-                {{ item.ukrdcid }}
-              </TextP>
-            </div>
-            <!-- Details (large breakpoint only) -->
-            <div class="hidden lg:block">
-              <TextL1>Last updated</TextL1>
-              <TextP class="mt-2">
-                {{ formatDate(item.repositoryUpdateDate) }}
-              </TextP>
-            </div>
-          </div>
+    <div class="flex items-center py-4">
+      <div class="min-w-0 flex-1 flex items-center">
+        <div class="flex-none justify-center w-16 cursor-pointer" @click="showDetail = !showDetail">
+          <IconChevronDown v-show="showDetail" class="mx-auto" />
+          <IconChevronRight v-show="!showDetail" class="mx-auto" />
         </div>
-        <div>
-          <IconChevronRight />
+        <div class="min-w-0 grid grid-cols-4 items-center md:gap-4 w-full pr-4">
+          <!-- Name, DoB, gender -->
+          <div>
+            <TextL1c class="capitalize truncate">
+              {{ item.sendingfacility }}
+            </TextL1c>
+            <TextP class="mt-2"> via {{ item.sendingextract }} </TextP>
+          </div>
+          <!-- National ID -->
+          <div>
+            <TextL1>Local ID</TextL1>
+            <TextP class="mt-2">
+              {{ item.localpatientid }}
+            </TextP>
+          </div>
+          <!-- UKRDC ID -->
+          <div>
+            <TextL1>UKRDC ID</TextL1>
+            <TextP class="mt-2">
+              {{ item.ukrdcid }}
+            </TextP>
+          </div>
+          <!-- Record link -->
+          <GenericButtonMini class="w-24 h-8 justify-self-end"
+            ><NuxtLink :to="`/patientrecords/${item.pid}`" class="block hover:bg-gray-50"
+              >View Record</NuxtLink
+            ></GenericButtonMini
+          >
         </div>
       </div>
-    </NuxtLink>
+    </div>
+    <!-- Collapsible details -->
+    <div v-show="showDetail" class="pl-16 pr-4 mb-4">
+      <PatientrecordsDetailCards :record="item" />
+    </div>
   </li>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 import { formatDate } from '@/utilities/dateUtils'
 import { PatientRecordShort } from '@/interfaces/patientrecord'
 
@@ -55,7 +57,8 @@ export default defineComponent({
     },
   },
   setup() {
-    return { formatDate }
+    const showDetail = ref(false)
+    return { showDetail, formatDate }
   },
 })
 </script>
