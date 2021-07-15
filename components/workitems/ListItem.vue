0ab9ced582cf82e2ad58f41f57aa2c04581f30f2
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-15 15:14:29 +0000

    Removed unused container div

--- components/workitems/ListItem.vue
+++ components/workitems/ListItem.vue
@@ -1,42 +1,40 @@
 <template>
   <li>
-    <div class="flex items-center px-4 py-4 sm:px-6">
-      <div class="min-w-0 grid grid-cols-3 lg:grid-cols-4 md:gap-4 w-full">
-        <!-- IDs and description -->
-        <div class="col-span-2">
-          <TextL1 v-if="item.person" class="capitalize truncate inline">
-            {{ item.person.givenname.toLowerCase() }}
-            {{ item.person.surname.toLowerCase() }} →</TextL1
-          >
-          <TextL1c v-if="item.masterRecord" class="capitalize truncate inline">
-            {{ item.masterRecord.givenname.toLowerCase() }}
-            {{ item.masterRecord.surname.toLowerCase() }}
-          </TextL1c>
-          <TextP class="mt-2 truncate">
-            {{ item.description }}
-          </TextP>
-        </div>
-        <!-- National ID -->
-        <div class="text-right sm:text-left">
-          <TextP v-if="item.masterRecord">
-            {{ item.masterRecord.nationalid.trim() }}
-          </TextP>
-          <TextP v-else-if="item.person">
-            {{ item.person.localid.trim() }}
-          </TextP>
-          <masterrecordsNationalIdTypeTag
-            v-if="item.masterRecord"
-            class="mt-2"
-            :nationalid-type="item.masterRecord.nationalidType"
-          />
-        </div>
-        <!-- Last updated (small and up) -->
-        <div class="hidden lg:block">
-          <TextP>From {{ facility }}</TextP>
-          <TextP class="mt-2">
-            {{ formatDate(item.lastUpdated) }}
-          </TextP>
-        </div>
+    <div class="px-4 py-4 sm:px-6 min-w-0 grid grid-cols-3 lg:grid-cols-4 md:gap-4 w-full">
+      <!-- IDs and description -->
+      <div class="col-span-2">
+        <TextL1 v-if="item.person" class="capitalize truncate inline">
+          {{ item.person.givenname.toLowerCase() }}
+          {{ item.person.surname.toLowerCase() }} →</TextL1
+        >
+        <TextL1c v-if="item.masterRecord" class="capitalize truncate inline">
+          {{ item.masterRecord.givenname.toLowerCase() }}
+          {{ item.masterRecord.surname.toLowerCase() }}
+        </TextL1c>
+        <TextP class="mt-2 truncate">
+          {{ item.description }}
+        </TextP>
+      </div>
+      <!-- National ID -->
+      <div class="text-right sm:text-left">
+        <TextP v-if="item.masterRecord">
+          {{ item.masterRecord.nationalid.trim() }}
+        </TextP>
+        <TextP v-else-if="item.person">
+          {{ item.person.localid.trim() }}
+        </TextP>
+        <masterrecordsNationalIdTypeTag
+          v-if="item.masterRecord"
+          class="mt-2"
+          :nationalid-type="item.masterRecord.nationalidType"
+        />
+      </div>
+      <!-- Last updated (small and up) -->
+      <div class="hidden lg:block">
+        <TextP>From {{ facility }}</TextP>
+        <TextP class="mt-2">
+          {{ formatDate(item.lastUpdated) }}
+        </TextP>
       </div>
     </div>
   </li>
