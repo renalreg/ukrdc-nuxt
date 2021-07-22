7e6caa8e5c16e2d9a5c2e95a65d1869e4053ea3b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-22 09:29:17 +0000

    Added missing plugin file

---
+++ plugins/vuex-persistedstate.ts
@@ -0,0 +1,18 @@
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
+
+import { Context } from '@nuxt/types'
+
+export default ({ store, isDev, req }: Context) => {
+  createPersistedState({
+    key: 'vuex.persisted_state.prefs',
+    paths: ['prefs'],
+    storage: {
+      getItem: (key) => (process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key]),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: !isDev }),
+      removeItem: (key) => Cookies.remove(key),
+    },
+  })(store)
+  // }
+}
