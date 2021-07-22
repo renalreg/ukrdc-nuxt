0fb54a2730400ec68f09d413ec37cd00ca36bd11
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-22 10:38:07 +0000

    Exclude ExpiredAuthSessionError from error handlers

--- plugins/axios-sentry.ts
+++ plugins/axios-sentry.ts
@@ -2,9 +2,13 @@
 Automatically send Axios error reports to Sentry.
 */
 import { Context } from '@nuxt/types'
+import { ExpiredAuthSessionError } from '@nuxtjs/auth-next/dist/runtime'
 
 export default function ({ $axios, $sentry }: Context) {
   $axios.onError((error) => {
+    if (error instanceof ExpiredAuthSessionError) {
+      return
+    }
     console.log('Error reported to developers via Sentry')
     $sentry.captureException(error)
   })
--- plugins/axios-toast.ts
+++ plugins/axios-toast.ts
@@ -2,9 +2,13 @@
 Automatically display Axios error toasts.
 */
 import { Context } from '@nuxt/types'
+import { ExpiredAuthSessionError } from '@nuxtjs/auth-next/dist/runtime'
 
 export default function ({ app, $axios }: Context) {
   $axios.onError((error) => {
+    if (error instanceof ExpiredAuthSessionError) {
+      return
+    }
     app.$toast.show({
       type: 'danger',
       title: 'Error Fetching Data',
