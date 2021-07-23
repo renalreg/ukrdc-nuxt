d12c8b0369c6d576c6460ec0eb58c0921a5579bc
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-23 14:43:13 +0000

    Enabled EMPI Merge API call logic

--- pages/empi/merge.vue
+++ pages/empi/merge.vue
@@ -6,7 +6,7 @@
       message="Are you sure you want to begin merging these records?"
       icon="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
       :danger="true"
-      @confirm=""
+      @confirm="requestMerge()"
     />
 
     <div class="max-w-7xl mx-auto mb-4">
@@ -17,11 +17,13 @@
       <div class="flex-1">
         <div v-if="superceeded">
           <GenericButton class="w-full" @click="clearSuperceeded"> Change Superceeded Record </GenericButton>
-          <masterrecordsRecordCard
-            class="mt-4 border-2 border-indigo-500"
-            :record="superceeded"
-            :label="`Superceeded ${superceeded.id.toString()}`"
-          />
+          <NuxtLink :to="`/masterrecords/${superceeded.id}`">
+            <masterrecordsRecordCard
+              class="mt-4 border-2 border-indigo-500"
+              :record="superceeded"
+              :label="`Superceeded ${superceeded.id.toString()}`"
+            />
+          </NuxtLink>
         </div>
         <div v-else>
           <EmpiSearch v-if="searchingFor === 'superceeded'" @select="selectSuperceeded" />
@@ -38,17 +40,19 @@
       </div>
 
       <div class="flex-1">
-        <div v-if="superceeding">
-          <GenericButton class="w-full" @click="clearSuperceeding"> Change Superceeding Record </GenericButton>
-          <masterrecordsRecordCard
-            class="mt-4 border-2 border-indigo-500"
-            :record="superceeding"
-            :label="`Superceeding ${superceeding.id.toString()}`"
-          />
+        <div v-if="superceding">
+          <GenericButton class="w-full" @click="clearsuperceding"> Change superceding Record </GenericButton>
+          <NuxtLink :to="`/masterrecords/${superceding.id}`">
+            <masterrecordsRecordCard
+              class="mt-4 border-2 border-indigo-500"
+              :record="superceding"
+              :label="`superceding ${superceding.id.toString()}`"
+            />
+          </NuxtLink>
         </div>
         <div v-else>
-          <EmpiSearch v-if="searchingFor === 'superceeding'" @select="selectSuperceeding" />
-          <GenericButton v-else class="w-full" @click="searchingFor = 'superceeding'">
+          <EmpiSearch v-if="searchingFor === 'superceding'" @select="selectsuperceding" />
+          <GenericButton v-else class="w-full" @click="searchingFor = 'superceding'">
             Search for a Record
           </GenericButton>
         </div>
@@ -62,17 +66,17 @@
         <TextH2 class="mb-2">Merge Details</TextH2>
         <TextP
           >Master Record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be merged into Master Record
-          <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          <TextL1c class="inline">{{ supercedingId }}</TextL1c
           >.
         </TextP>
         <TextP
           >Demographic data on record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be replaced by
-          demographic data from record <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          demographic data from record <TextL1c class="inline">{{ supercedingId }}</TextL1c
           >.</TextP
         >
         <TextP
           >All Patient Records linked to Master Record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be
-          re-linked to Master Record <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          re-linked to Master Record <TextL1c class="inline">{{ supercedingId }}</TextL1c
           >.</TextP
         >
       </div>
@@ -89,7 +93,7 @@ import { MasterRecord } from '~/interfaces/masterrecord'
 import useQuery from '~/mixins/useQuery'
 
 enum Direction {
-  Superceeding = 'superceeding',
+  superceding = 'superceding',
   Superceeded = 'superceeded',
 }
 
@@ -97,26 +101,26 @@ export default defineComponent({
   setup() {
     const route = useRoute()
     const router = useRouter()
-    const { $axios, $config } = useContext()
+    const { $axios, $config, $toast } = useContext()
     const { stringQuery } = useQuery()
 
     const beginMergeAlert = ref<modalInterface>()
 
     const superceededId = stringQuery('superceeded', null, true, false)
-    const superceedingId = stringQuery('superceeding', null, true, false)
+    const supercedingId = stringQuery('superceding', null, true, false)
 
     const superceeded = ref<MasterRecord>()
-    const superceeding = ref<MasterRecord>()
+    const superceding = ref<MasterRecord>()
 
     const searchingFor = ref<Direction>()
 
     const readyToMerge = computed(() => {
-      return superceeded.value?.id && superceeding.value?.id && superceeded.value?.id !== superceeding.value?.id
+      return superceeded.value?.id && superceding.value?.id && superceeded.value?.id !== superceding.value?.id
     })
 
     const mergeBlockDescription = computed(() => {
-      if (superceeded.value?.id || superceeding.value?.id) {
-        if (superceeded.value?.id === superceeding.value?.id) {
+      if (superceeded.value?.id || superceding.value?.id) {
+        if (superceeded.value?.id === superceding.value?.id) {
           return 'A record cannot be merged into itself. Please select a different record on one side.'
         }
       }
@@ -125,18 +129,59 @@ export default defineComponent({
 
     function switchRecords() {
       const newQuery = Object.assign({}, route.value.query)
-      newQuery.superceeded = [superceedingId.value]
-      newQuery.superceeding = [superceededId.value]
+      newQuery.superceeded = [supercedingId.value]
+      newQuery.superceding = [superceededId.value]
       router.push({
         path: route.value.path,
         query: newQuery,
       })
     }
 
-    function clearSuperceeding() {
-      superceeding.value = undefined
-      superceedingId.value = null
-      searchingFor.value = Direction.Superceeding
+    function clearMerge() {
+      superceding.value = undefined
+      superceeded.value = undefined
+      searchingFor.value = undefined
+      const newQuery = Object.assign({}, route.value.query)
+      newQuery.superceeded = [null]
+      newQuery.superceding = [null]
+      router.push({
+        path: route.value.path,
+        query: newQuery,
+      })
+    }
+
+    async function requestMerge() {
+      try {
+        await $axios.$post(`${$config.apiBase}/v1/empi/merge`, {
+          superceding: superceding.value?.id,
+          superceeded: superceeded.value?.id,
+        })
+        $toast.show({
+          type: 'success',
+          title: 'Success',
+          message: 'Record merge request sent successfully',
+          timeout: 10,
+          classTimeout: 'bg-green-600',
+        })
+        clearMerge()
+      } catch (error) {
+        console.log(error.response.data.detail)
+        $toast.show({
+          type: 'danger',
+          title: 'Error',
+          message: 'Error processing merge request',
+          timeout: 10,
+          classTimeout: 'bg-red-600',
+        })
+      }
+      const el = beginMergeAlert.value as modalInterface
+      el.hide()
+    }
+
+    function clearsuperceding() {
+      superceding.value = undefined
+      supercedingId.value = null
+      searchingFor.value = Direction.superceding
     }
 
     function clearSuperceeded() {
@@ -145,8 +190,8 @@ export default defineComponent({
       searchingFor.value = Direction.Superceeded
     }
 
-    function selectSuperceeding(id: string) {
-      superceedingId.value = id
+    function selectsuperceding(id: string) {
+      supercedingId.value = id
     }
 
     function selectSuperceeded(id: string) {
@@ -162,28 +207,29 @@ export default defineComponent({
     const { fetch } = useFetch(async () => {
       await Promise.all([
         superceededId.value ? fetchRecord(superceededId.value, superceeded) : null,
-        superceedingId.value ? fetchRecord(superceedingId.value, superceeding) : null,
+        supercedingId.value ? fetchRecord(supercedingId.value, superceding) : null,
       ])
     })
 
-    watch([superceededId, superceedingId], () => {
+    watch([superceededId, supercedingId], () => {
       fetch()
     })
 
     return {
       beginMergeAlert,
       superceededId,
-      superceedingId,
+      supercedingId,
       superceeded,
-      superceeding,
+      superceding,
       searchingFor,
-      clearSuperceeding,
+      clearsuperceding,
       clearSuperceeded,
-      selectSuperceeding,
+      selectsuperceding,
       selectSuperceeded,
       switchRecords,
       readyToMerge,
       mergeBlockDescription,
+      requestMerge,
     }
   },
 })
