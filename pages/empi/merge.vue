12c61b6beb82a7f753c9f510a7008b9c2480b55d
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-23 13:19:54 +0000

    Added merge description and confirmation modal

--- pages/empi/merge.vue
+++ pages/empi/merge.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
+    <GenericModalConfirm
+      ref="beginMergeAlert"
+      title="Begin Record Merge"
+      message="Are you sure you want to begin merging these records?"
+      icon="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+      :danger="true"
+      @confirm=""
+    />
+
     <div class="max-w-7xl mx-auto mb-4">
-      <h1 class="text-2xl font-semibold text-gray-900">Merge Records</h1>
+      <TextH1>Merge Records</TextH1>
     </div>
 
-    <div class="block lg:flex gap-2 mb-4">
+    <div class="block lg:flex gap-2 mb-6">
       <div class="flex-1">
         <div v-if="superceeded">
           <GenericButton class="w-full" @click="clearSuperceeded"> Change Superceeded Record </GenericButton>
           <masterrecordsRecordCard
-            class="mt-2 border-2 border-indigo-500"
+            class="mt-4 border-2 border-indigo-500"
             :record="superceeded"
             :label="`Superceeded ${superceeded.id.toString()}`"
           />
@@ -32,7 +41,7 @@
         <div v-if="superceeding">
           <GenericButton class="w-full" @click="clearSuperceeding"> Change Superceeding Record </GenericButton>
           <masterrecordsRecordCard
-            class="mt-2 border-2 border-indigo-500"
+            class="mt-4 border-2 border-indigo-500"
             :record="superceeding"
             :label="`Superceeding ${superceeding.id.toString()}`"
           />
@@ -45,12 +54,37 @@
         </div>
       </div>
     </div>
+
+    <GenericAlertWarning v-if="mergeBlockDescription" :message="mergeBlockDescription" />
+
+    <div v-if="readyToMerge">
+      <div class="mb-6">
+        <TextH2 class="mb-2">Merge Details</TextH2>
+        <TextP
+          >Master Record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be merged into Master Record
+          <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          >.
+        </TextP>
+        <TextP
+          >Demographic data on record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be replaced by
+          demographic data from record <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          >.</TextP
+        >
+        <TextP
+          >All Patient Records linked to Master Record <TextL1c class="inline">{{ superceededId }}</TextL1c> will be
+          re-linked to Master Record <TextL1c class="inline">{{ superceedingId }}</TextL1c
+          >.</TextP
+        >
+      </div>
+      <GenericButton colour="red" @click="beginMergeAlert.show()">Begin Record Merge</GenericButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Ref, ref, useContext, useFetch, useRoute, useRouter, watch } from '@nuxtjs/composition-api'
+import { computed, Ref, ref, useContext, useFetch, useRoute, useRouter, watch } from '@nuxtjs/composition-api'
 import { defineComponent } from '@vue/composition-api'
+import { modalInterface } from '@/interfaces/modal'
 import { MasterRecord } from '~/interfaces/masterrecord'
 import useQuery from '~/mixins/useQuery'
 
@@ -66,6 +100,8 @@ export default defineComponent({
     const { $axios, $config } = useContext()
     const { stringQuery } = useQuery()
 
+    const beginMergeAlert = ref<modalInterface>()
+
     const superceededId = stringQuery('superceeded', null, true, false)
     const superceedingId = stringQuery('superceeding', null, true, false)
 
@@ -73,6 +109,17 @@ export default defineComponent({
     const superceeding = ref<MasterRecord>()
 
     const searchingFor = ref<Direction>()
+
+    const readyToMerge = computed(() => {
+      return superceededId.value && superceedingId.value && superceededId.value !== superceedingId.value
+    })
+
+    const mergeBlockDescription = computed(() => {
+      if (superceededId.value === superceedingId.value) {
+        return 'A record cannot be merged into itself. Please select a different record on one side.'
+      }
+      return ''
+    })
 
     function switchRecords() {
       const newQuery = Object.assign({}, route.value.query)
@@ -122,6 +169,7 @@ export default defineComponent({
     })
 
     return {
+      beginMergeAlert,
       superceededId,
       superceedingId,
       superceeded,
@@ -132,6 +180,8 @@ export default defineComponent({
       selectSuperceeding,
       selectSuperceeded,
       switchRecords,
+      readyToMerge,
+      mergeBlockDescription,
     }
   },
 })
