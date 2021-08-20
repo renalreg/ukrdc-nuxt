3cec432e6696aecd040e1d545460359e3059d364
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-20 15:40:27 +0000

    More sensible indentation

--- components/workitems/AdviceCard.vue
+++ components/workitems/AdviceCard.vue
@@ -7,10 +7,10 @@
           <TextP>Related Work Items labelled UKRDC should be resolved first. See below.</TextP>
         </li>
         <li v-if="workItemAdvices.includes(4)">
-          <TextP
-            >You may need to use PatientView, RaDaR, or DemoGraphicGenerator.exe to issue a demographic update before
-            closing this Work Item.</TextP
-          >
+          <TextP>
+            You may need to use PatientView, RaDaR, or DemoGraphicGenerator.exe to issue a demographic update before
+            closing this Work Item.
+          </TextP>
         </li>
         <li v-if="workItemAdvices.includes(5)">
           <TextP class="mb-1">
@@ -26,20 +26,21 @@
           <TextP>This Work Item was recently merged, and can now be closed.</TextP>
         </li>
         <li v-if="workItemAdvices.includes(7)">
-          <TextP
-            >A previous merge may have been completed but Person record data was not correctly updated to match.</TextP
-          >
+          <TextP>
+            A previous merge may have been completed but Person record data was not correctly updated to match.
+          </TextP>
         </li>
         <li v-if="workItemAdvices.includes(9)">
-          <TextP
-            >See
+          <TextP>
+            See
             <a
               href="https://confluence.ukrdc.org/display/TNG/Person+matched+by+facility%2C+extract+and+national+id+-+not+matched+by+demographics"
               target="_blank"
-              >documentation on Confluence</a
             >
-            for advice on resolving this work item.</TextP
-          >
+              documentation on Confluence
+            </a>
+            for advice on resolving this work item.
+          </TextP>
         </li>
         <li v-if="workItemAdvices.includes(10)">
           <TextP>This Work Item is already closed. No further action to be taken.</TextP>
