7d6d1c280c2f7468392618d5df571f299a4caa2c
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-23 11:55:35 +0000

    Fixed viewBox

--- components/icon/mini/ArrowRight.vue
+++ components/icon/mini/ArrowRight.vue
@@ -1,10 +1,5 @@
 <template>
-  <svg
-    class="h-6 mx-auto my-2 text-gray-400"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-  >
+  <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
     <path
       fill-rule="evenodd"
       d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
