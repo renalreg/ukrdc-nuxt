ec97408a5206648ae9d85d4eaaf5b7a533299da7
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-09-07 15:24:17 +0000

    Inverted error filter logic

--- plugins/axios-sentry.ts
+++ plugins/axios-sentry.ts
@@ -7,10 +7,9 @@ import { ExpiredAuthSessionError } from '@nuxtjs/auth-next/dist/runtime'
 export default function ({ $axios, $sentry }: Context) {
   $axios.onError((error) => {
     // Let nuxt-auth handle logging out on ExpiredAuthSessionError
-    if (error instanceof ExpiredAuthSessionError) {
-      return
+    if (!(error instanceof ExpiredAuthSessionError)) {
+      console.log('Error reported to developers via Sentry')
+      $sentry.captureException(error)
     }
-    console.log('Error reported to developers via Sentry')
-    $sentry.captureException(error)
   })
 }
--- plugins/axios-toast.ts
+++ plugins/axios-toast.ts
@@ -7,14 +7,13 @@ import { ExpiredAuthSessionError } from '@nuxtjs/auth-next/dist/runtime'
 export default function ({ app, $axios }: Context) {
   $axios.onError((error) => {
     // Let nuxt-auth handle logging out on ExpiredAuthSessionError
-    if (error instanceof ExpiredAuthSessionError) {
-      return
+    if (!(error instanceof ExpiredAuthSessionError)) {
+      app.$toast.show({
+        type: 'danger',
+        title: 'Error Fetching Data',
+        message: error.response ? error.response.data.detail : error.message,
+        timeout: 5,
+      })
     }
-    app.$toast.show({
-      type: 'danger',
-      title: 'Error Fetching Data',
-      message: error.response ? error.response.data.detail : error.message,
-      timeout: 5,
-    })
   })
 }
