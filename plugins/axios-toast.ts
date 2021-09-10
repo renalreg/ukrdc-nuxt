940ce3260e728405253362b382ce9b335caa0dc6
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-09-10 08:40:24 +0000

    Ignore toast if no message string is available

--- plugins/axios-toast.ts
+++ plugins/axios-toast.ts
@@ -8,12 +8,17 @@ export default function ({ app, $axios }: Context) {
   $axios.onError((error) => {
     // Let nuxt-auth handle logging out on ExpiredAuthSessionError
     if (!(error instanceof ExpiredAuthSessionError)) {
-      app.$toast.show({
-        type: 'danger',
-        title: 'Error Fetching Data',
-        message: error.response ? error.response.data.detail : error.message,
-        timeout: 5,
-      })
+      // Compute what message to show in the toast
+      const msgToShow = error.response ? error.response.data.detail : error.message
+      // If we have no message to show, don't create a toast
+      if (msgToShow) {
+        app.$toast.show({
+          type: 'danger',
+          title: 'Error Fetching Data',
+          message: error.response ? error.response.data.detail : error.message,
+          timeout: 5,
+        })
+      }
     }
   })
 }
