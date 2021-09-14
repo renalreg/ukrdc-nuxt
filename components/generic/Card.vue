180b0a65b9060b6c6af5f33a1924480948d6e279
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-09-14 14:51:39 +0000

    Fixed card overflow

--- components/generic/Card.vue
+++ components/generic/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white border border-gray-300 shadow-sm rounded-md">
+  <div class="bg-white border border-gray-300 shadow-sm rounded-md overflow-hidden">
     <slot />
   </div>
 </template>
