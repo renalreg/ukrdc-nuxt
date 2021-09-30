a9e62eb86746bd6d99243573ca1d0d78836cc42c
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-09-30 15:29:25 +0000

    Added generic textbox component

---
+++ components/form/TextBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    class="
+      block
+      w-full
+      bg-white
+      border border-gray-300
+      rounded-md
+      py-2
+      pl-10
+      pr-3
+      placeholder-gray-500
+      focus:outline-none
+      focus:text-gray-900
+      focus:placeholder-gray-400
+      focus:ring-1
+      focus:ring-indigo-500
+      focus:border-indigo-500
+      sm:
+    "
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    value: {
+      default: '',
+      type: String,
+    },
+  },
+})
+</script>
