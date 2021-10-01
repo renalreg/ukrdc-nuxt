ba939d26c72cb874e846228a652e94bd903f6c49
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-10-01 14:18:21 +0000

    Add familydoctor and GPInfo

--- interfaces/patient.ts
+++ interfaces/patient.ts
@@ -19,10 +19,53 @@ interface PatientAddress {
   countryDescription: string
 }
 
+interface ContactDetails {
+  use: string
+  value: string
+  commenttext: string
+}
+
+interface GPInfo {
+  code: string
+  gpname: string
+  street: string
+  postcode: string
+  contactvalue: string
+  type: string
+}
+
+interface FamilyDoctor {
+  id: string
+  gpname: string
+
+  gpid: string
+  gpInfo: GPInfo
+
+  gppracticeid: string
+  gpPracticeInfo: GPInfo
+
+  addressuse: string
+  fromtime: string
+  totime: string
+  town: string
+  county: string
+  postcode: string
+  countrycode: string
+  countrycodestd: string
+  countrydesc: string
+  contactuse: string
+  contactvalue: string
+  email: string
+  commenttext: string
+}
+
 export interface Patient {
   names: PatientName[]
   numbers: PatientNumber[]
   addresses: PatientAddress[]
+  contactdetails: ContactDetails[]
+
+  familydoctor: FamilyDoctor
 
   birthTime: string
   deathTime: string
