91e39bf653b00683d6f8abfee6324caee1c9e95b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-11-10 15:30:44 +0000

    Added PKB data flow card

--- components/facilities/Overview.vue
+++ components/facilities/Overview.vue
@@ -1,133 +1,167 @@
 <template>
   <div>
-    <LoadingIndicator v-if="!facility"></LoadingIndicator>
-    <div v-else>
-      <div v-if="showHeading" class="mb-6">
-        <TextH1 v-if="facility"> {{ facility.description }} </TextH1>
-        <SkeleText v-else class="h-8 w-1/4 mb-2" />
-        <TextL1 v-if="facility">
-          {{ facility.id }}
-        </TextL1>
-        <SkeleText v-else class="h-4 w-1/2" />
-      </div>
+    <div class="mb-4">
+      <TextH1 v-if="facility"> {{ facility.description }} </TextH1>
+      <SkeleText v-else class="h-8 w-1/4 mb-2" />
+      <TextL1 v-if="facility">
+        {{ facility.id }}
+      </TextL1>
+      <SkeleText v-else class="h-4 w-1/2" />
+    </div>
 
-      <div v-if="!facility.statistics.lastUpdated">
-        <TextP>
-          No data available. This usually means the service has recently been restarted, and is rebuilding statistics.
-        </TextP>
-        <TextP> Please try again in a few minutes. </TextP>
-      </div>
+    <div v-if="facility && !facility.statistics.lastUpdated">
+      <TextP>
+        No data available. This usually means the service has recently been restarted, and is rebuilding statistics.
+      </TextP>
+      <TextP> Please try again in a few minutes. </TextP>
+    </div>
 
-      <div v-else>
-        <TextL2 class="mb-2"> Last updated {{ lastUpdatedString }} </TextL2>
-        <!-- Basic stats -->
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-          <GenericCard>
-            <div class="flex items-center p-4">
-              <div class="flex-shrink-0">
-                <IconUsers />
-              </div>
-              <div class="ml-5 w-0 flex-1">
-                <dl>
-                  <GenericCardDt>Total Patients</GenericCardDt>
-                  <dd>
-                    <TextHc>{{ facility.statistics.totalPatients }}</TextHc>
-                  </dd>
-                </dl>
-              </div>
+    <div v-else-if="facility && facility.statistics.lastUpdated">
+      <TextL2 class="mb-2 -mt-4 text-right"> Last updated {{ lastUpdatedString }} </TextL2>
+      <!-- Basic stats -->
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+        <GenericCard>
+          <div class="flex items-center p-4">
+            <div class="flex-shrink-0">
+              <IconUsers />
             </div>
-            <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
-              Total patients ever stored in the UKRDC for this facility
+            <div class="ml-5 w-0 flex-1">
+              <dl>
+                <GenericCardDt>Total Patients</GenericCardDt>
+                <dd>
+                  <TextHc>{{ facility.statistics.totalPatients }}</TextHc>
+                </dd>
+              </dl>
             </div>
-          </GenericCard>
-
-          <GenericCard>
-            <div class="flex items-center p-4">
-              <div class="flex-shrink-0">
-                <IconCheck />
-              </div>
-              <div class="ml-5 w-0 flex-1">
-                <dl>
-                  <GenericCardDt>Active Passing Records</GenericCardDt>
-                  <dd>
-                    <h1 class="text-2xl font-semibold text-green-600">
-                      {{ facility.statistics.patientsReceivingMessageSuccess }}
-                    </h1>
-                  </dd>
-                </dl>
-              </div>
-            </div>
-            <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
-              Records with active data files being successfully received
-            </div>
-          </GenericCard>
-
-          <GenericCard>
-            <div class="flex items-center p-4">
-              <div class="flex-shrink-0">
-                <IconExclamation />
-              </div>
-              <div class="ml-5 w-0 flex-1">
-                <dl>
-                  <GenericCardDt>Active Failing Records</GenericCardDt>
-                  <dd>
-                    <h1
-                      class="text-2xl font-semibold"
-                      :class="facility.statistics.patientsReceivingMessageError > 0 ? 'text-red-600' : 'text-green-600'"
-                    >
-                      {{ facility.statistics.patientsReceivingMessageError }}
-                    </h1>
-                  </dd>
-                </dl>
-              </div>
-            </div>
-            <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
-              Records with active data files currently failing due to errors
-            </div>
-          </GenericCard>
-        </div>
-
-        <!-- Error history -->
-        <GenericCard v-if="facilityErrorsHistory">
-          <GenericCardHeader>
-            <TextH2> Error History </TextH2>
-          </GenericCardHeader>
-          <GenericCardContent class="p-4">
-            <ChartTimeSeries
-              class="h-64"
-              :data="facilityErrorsHistory"
-              label="New Errors"
-              @click="historyPointClickHandler"
-            />
-          </GenericCardContent>
+          </div>
+          <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
+            Total patients ever stored in the UKRDC for this facility
+          </div>
         </GenericCard>
 
-        <!-- Failing NIs -->
-        <GenericCard v-if="facility.statistics.patientsReceivingMessageError > 0" class="mt-4">
-          <GenericCardHeader>
-            <TextH2> Records Currently Failing </TextH2>
-            <TextL1>Records where the most recent message received failed to process due to errors.</TextL1>
-          </GenericCardHeader>
-          <ul class="divide-y divide-gray-200">
-            <div v-for="item in errorMessages" :key="item.id" :item="item" class="hover:bg-gray-50">
-              <NuxtLink :to="`/messages/${item.id}`">
-                <MessagesListItem :item="item" />
-              </NuxtLink>
+        <GenericCard>
+          <div class="flex items-center p-4">
+            <div class="flex-shrink-0">
+              <IconCheck />
             </div>
-          </ul>
-          <GenericPaginator
-            class="bg-white border-t border-gray-200"
-            :jump-to-top="false"
-            :page="errorMessagesPage"
-            :size="errorMessagesSize"
-            :total="errorMessagesTotal"
-            @next="errorMessagesPage++"
-            @prev="errorMessagesPage--"
-            @jump="errorMessagesPage = $event"
-          />
+            <div class="ml-5 w-0 flex-1">
+              <dl>
+                <GenericCardDt>Active Passing Records</GenericCardDt>
+                <dd>
+                  <h1 class="text-2xl font-semibold text-green-600">
+                    {{ facility.statistics.patientsReceivingMessageSuccess }}
+                  </h1>
+                </dd>
+              </dl>
+            </div>
+          </div>
+          <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
+            Records with active data files being successfully received
+          </div>
+        </GenericCard>
+
+        <GenericCard>
+          <div class="flex items-center p-4">
+            <div class="flex-shrink-0">
+              <IconExclamation />
+            </div>
+            <div class="ml-5 w-0 flex-1">
+              <dl>
+                <GenericCardDt>Active Failing Records</GenericCardDt>
+                <dd>
+                  <h1
+                    class="text-2xl font-semibold"
+                    :class="facility.statistics.patientsReceivingMessageError > 0 ? 'text-red-600' : 'text-green-600'"
+                  >
+                    {{ facility.statistics.patientsReceivingMessageError }}
+                  </h1>
+                </dd>
+              </dl>
+            </div>
+          </div>
+          <div class="bg-gray-50 text-gray-500 px-4 py-2 text-sm">
+            Records with active data files currently failing due to errors
+          </div>
         </GenericCard>
       </div>
     </div>
+
+    <!-- Data flow -->
+    <GenericCard v-if="facility && facility.dataFlow" class="mb-4">
+      <GenericCardContent>
+        <GenericDl>
+          <GenericDi>
+            <TextDt>Sending data to Patients Know Best</TextDt>
+            <TextDd>
+              <div v-if="facility && facility.dataFlow.pkbOut" class="flex items-center gap-2">
+                <div>Yes</div>
+                <IconCheckCircle class="inline text-green-600" />
+              </div>
+              <div v-else>No</div>
+            </TextDd>
+          </GenericDi>
+          <GenericDi>
+            <TextDt>Receiving data from Patients Know Best</TextDt>
+            <TextDd>
+              <div v-if="facility && facility.dataFlow.pkbIn" class="flex items-center gap-2">
+                <div>Yes</div>
+                <IconCheckCircle class="inline text-green-600" />
+              </div>
+              <div v-else>No</div>
+            </TextDd>
+          </GenericDi>
+          <GenericDi>
+            <TextDt>Excluding data from Patients Know Best</TextDt>
+            <TextDd>
+              <div v-if="facility && facility.dataFlow.pkbMessageExclusions.length > 0" class="flex items-center gap-2">
+                <div>{{ facility.dataFlow.pkbMessageExclusions.join(', ') }}</div>
+              </div>
+              <div v-else>None</div>
+            </TextDd>
+          </GenericDi>
+        </GenericDl>
+      </GenericCardContent>
+    </GenericCard>
+
+    <!-- Error history -->
+    <GenericCard v-if="facilityErrorsHistory">
+      <GenericCardHeader>
+        <TextH2> Error History </TextH2>
+      </GenericCardHeader>
+      <GenericCardContent class="p-4">
+        <ChartTimeSeries
+          class="h-64"
+          :data="facilityErrorsHistory"
+          label="New Errors"
+          @click="historyPointClickHandler"
+        />
+      </GenericCardContent>
+    </GenericCard>
+
+    <!-- Failing NIs -->
+    <GenericCard v-if="errorMessages && errorMessagesTotal > 0" class="mt-4">
+      <GenericCardHeader>
+        <TextH2> Records Currently Failing </TextH2>
+        <TextL1>Records where the most recent message received failed to process due to errors.</TextL1>
+      </GenericCardHeader>
+      <ul class="divide-y divide-gray-200">
+        <div v-for="item in errorMessages" :key="item.id" :item="item" class="hover:bg-gray-50">
+          <NuxtLink :to="`/messages/${item.id}`">
+            <MessagesListItem :item="item" />
+          </NuxtLink>
+        </div>
+      </ul>
+      <GenericPaginator
+        class="bg-white border-t border-gray-200"
+        :jump-to-top="false"
+        :page="errorMessagesPage"
+        :size="errorMessagesSize"
+        :total="errorMessagesTotal"
+        @next="errorMessagesPage++"
+        @prev="errorMessagesPage--"
+        @jump="errorMessagesPage = $event"
+      />
+    </GenericCard>
   </div>
 </template>
 
@@ -150,11 +184,6 @@ export default defineComponent({
     code: {
       type: String,
       required: true,
-    },
-    showHeading: {
-      type: Boolean,
-      required: false,
-      default: true,
     },
   },
   setup(props) {
