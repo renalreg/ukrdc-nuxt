8aa7cd76e6d63002deccaf689ae3ada17103b44f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-12-08 17:11:21 +0000

    Add AuditEvent interface

---
+++ interfaces/audit.ts
@@ -0,0 +1,24 @@
+interface AccessEvent {
+  id: number
+  time: string
+
+  userId: string
+  clientId: string
+  userEmail: string
+
+  path: string
+  method: string
+  body: string
+}
+
+export interface AuditEvent {
+  id: number
+  accessEvent: AccessEvent
+
+  resource: string
+  resourceId: string
+
+  operation: string
+
+  children: AuditEvent[]
+}
