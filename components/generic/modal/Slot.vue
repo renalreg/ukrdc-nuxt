0737379ff395244642196f4ed6209c7646d7e24b
Joel Collins <me@jtcollins.net>
2022-01-13 17:18:36 +0000

    Improve slot modal layout

--- components/generic/modal/Slot.vue
+++ components/generic/modal/Slot.vue
@@ -1,17 +1,18 @@
 <template>
   <transition :duration="200">
     <div v-show="visible" class="fixed z-10 inset-0 overflow-y-auto">
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 mt-16 md:mt-0">
+      <!-- Use padding to push modal out from underneath the navbar and provide a region to close on small displays -->
+      <div
+        class="flex items-end sm:items-center justify-center px-4 pb-4 pt-32 md:px-4 md:py-4 text-center h-screen content-box"
+      >
         <!-- Background overlay, show/hide based on modal state. -->
         <GenericBlackout :visible="visible" @click="hide()" />
 
-        <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <!-- Modal panel, show/hide based on modal state. -->
         <TransitionModal>
           <div
             v-show="visible"
-            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6"
+            class="inline-block align-bottom bg-white rounded-lg p-4 sm:p-6 text-left shadow-xl transform transition-all sm:align-middle sm:max-w-xl w-full max-h-full overflow-auto"
             role="dialog"
             aria-modal="true"
             aria-labelledby="modal-headline"
