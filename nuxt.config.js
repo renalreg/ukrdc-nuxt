7d8b50797d35feb0d9b9ce3d5ea563c9423c99f8
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-01-20 16:20:39 +0000

    Add proper base page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ukrdc-nuxt',
+    title: 'UKRDC Web Client',
     htmlAttrs: {
       lang: 'en',
     },
