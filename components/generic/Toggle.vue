fb6f88caf3f11e3bb99b53a7befad71c821aae38
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-02-14 16:14:13 +0000

    Added toggle to exclude inactive facilities from list

--- components/generic/Toggle.vue
+++ components/generic/Toggle.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="flex items-center">
+  <div class="flex items-center gap-3" :class="rightToLeft ? 'flex-row-reverse' : 'flex-row'">
     <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
     <button
       type="button"
-      class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      class="
+        relative
+        inline-flex
+        h-6
+        w-11
+        flex-shrink-0
+        cursor-pointer
+        rounded-full
+        border-2 border-transparent
+        bg-gray-200
+        transition-colors
+        duration-200
+        ease-in-out
+        focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+      "
       :class="{ 'bg-green-400': value }"
       aria-pressed="false"
       aria-labelledby="toggle-label"
@@ -13,11 +27,25 @@
       <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
       <span
         aria-hidden="true"
-        class="pointer-events-none inline-block h-5 w-5 translate-x-0 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+        class="
+          pointer-events-none
+          inline-block
+          h-5
+          w-5
+          translate-x-0
+          transform
+          rounded-full
+          bg-white
+          shadow
+          ring-0
+          transition
+          duration-200
+          ease-in-out
+        "
         :class="{ 'translate-x-5': value }"
       ></span>
     </button>
-    <span v-if="label" id="toggle-label" class="ml-3">
+    <span v-if="label" id="toggle-label">
       <span class="font-medium text-gray-900">{{ label }} </span>
     </span>
   </div>
@@ -36,6 +64,11 @@ export default defineComponent({
     value: {
       type: Boolean,
       required: true,
+    },
+    rightToLeft: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
   setup(props, { emit }) {
