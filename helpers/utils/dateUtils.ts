65f3ba293cbd42445746dbc6397c13ab5ea5854f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-03-10 14:19:31 +0000

    Fix date of mirth tracing comparison

--- helpers/utils/dateUtils.ts
+++ helpers/utils/dateUtils.ts
@@ -10,6 +10,10 @@ export function formatDate(rawDate: string, t: boolean = true, s: boolean = fals
     .toLocaleString(t ? (s ? DateTime.DATETIME_SHORT_WITH_SECONDS : DateTime.DATETIME_SHORT) : DateTime.DATE_SHORT);
 }
 
+export function datesAreEqual(date1: string, date2: string): boolean {
+  return DateTime.fromISO(date1).startOf("day").toMillis() === DateTime.fromISO(date2).startOf("day").toMillis();
+}
+
 export interface DateRange {
   start: string | null;
   end: string | null;
