82bbc3952adbe13a55c4cb93080b40b392913e1e
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-05-18 15:32:57 +0000

    Simplified record grouping code

--- helpers/utils/recordUtils.ts
+++ helpers/utils/recordUtils.ts
@@ -2,8 +2,6 @@ import { PatientNumber } from "~/interfaces/patient";
 import { PatientRecordSummary } from "~/interfaces/patientrecord";
 
 const ukrdcMembershipFacilities = ["PV", "PKB"];
-const membershipExtracts = ["RADAR"];
-const membershipFacilities = ["UKRR"];
 const migratedExtracts = ["PVMIG", "HSMIG"];
 
 /*
@@ -16,24 +14,8 @@ export interface localNumber {
   number: string;
 }
 
-export function isData(record: PatientRecordSummary): boolean {
-  // All PV extracts are data
-  if (record.sendingextract === "PV") {
-    return true;
-  }
-  // UKRR/UKRR is a membership, but otherwise UKRR extracts are data
-  if (record.sendingextract === "UKRR" && !(record.sendingfacility === "UKRR")) {
-    return true;
-  }
-  // UKRDC extracts are data, unless facility is PV, PKB, or TRACING
-  if (record.sendingextract === "UKRDC" && !["PV", "PKB", "TRACING"].includes(record.sendingfacility)) {
-    return true;
-  }
-  return false;
-}
-
 export function isSurvey(record: PatientRecordSummary): boolean {
-  // All SURVEY extracts are surveys
+  // All SURVEY extracts are survey records
   if (record.sendingextract === "SURVEY") {
     return true;
   }
@@ -41,32 +23,43 @@ export function isSurvey(record: PatientRecordSummary): boolean {
 }
 
 export function isMigrated(record: PatientRecordSummary) {
-  // All *MIG extracts are migrated
+  // All *MIG extracts are migrated records
   if (migratedExtracts.includes(record.sendingextract)) {
     return true;
   }
   return false;
 }
 
+export function isTracing(record: PatientRecordSummary) {
+  // All TRACING facilities are tracing records
+  if (record.sendingfacility === "TRACING") {
+    return true;
+  }
+  return false;
+}
+
 export function isMembership(record: PatientRecordSummary) {
-  if (membershipExtracts.includes(record.sendingextract)) {
+  // All RADAR extracts are membership records
+  if (record.sendingextract === "RADAR") {
     return true;
   }
-  if (membershipFacilities.includes(record.sendingfacility)) {
+  // All UKRR facilities are membership records
+  if (record.sendingfacility === "UKRR") {
     return true;
   }
+  // UKRDC extracts are only memberships if they are from certain facilities
   if (record.sendingextract === "UKRDC" && ukrdcMembershipFacilities.includes(record.sendingfacility)) {
     return true;
   }
   return false;
 }
 
-export function isTracing(record: PatientRecordSummary) {
-  if (record.sendingextract === "UKRDC" && record.sendingfacility === "TRACING") {
-    return true;
-  }
-  return false;
+export function isData(record: PatientRecordSummary): boolean {
+  // Everything else is a data record
+  return !(isSurvey(record) || isMigrated(record) || isTracing(record) || isMembership(record));
 }
+
+// General record utilities
 
 export function firstForename(record: PatientRecordSummary): string {
   return record.patient.names[0]?.given || "";
