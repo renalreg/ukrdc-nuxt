9fa569039b4010f95af5bd7fdcb5fcda1138d154
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-08-12 23:27:30 +0000

    Added API helper

---
+++ helpers/useApi.ts
@@ -0,0 +1,39 @@
+import { useContext } from "@nuxtjs/composition-api";
+import { AdminApi, DashboardApi } from "@ukkidney/ukrdc-axios-ts";
+import axios from "axios";
+
+export default function () {
+  const { $okta, $config } = useContext();
+
+  const defaultHeaders = {
+    Accept: "application/json",
+  };
+
+  // Create a custom axios instance
+  const apiInstance = axios.create({
+    headers: defaultHeaders,
+  });
+
+  // Request interceptor to handle API base paths and authentication
+  apiInstance.interceptors.request.use((config) => {
+    // Add the authorization header to each request
+    const token = $okta.getAccessToken();
+    if (config.headers === undefined) {
+      config.headers = defaultHeaders;
+    }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  });
+
+  // Create API instances
+  const adminApi = new AdminApi(undefined, $config.api.host || "/", apiInstance);
+  const dashboardApi = new DashboardApi(undefined, $config.api.host || "/", apiInstance);
+
+  return {
+    adminApi,
+    dashboardApi,
+  };
+}
