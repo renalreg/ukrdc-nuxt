2055d4c7d9c6de90d8df93a0bf2a27ea198b992f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-08-16 19:00:31 +0000

    Correct relative baseUrl behaviour

--- helpers/useApi.ts
+++ helpers/useApi.ts
@@ -113,20 +113,27 @@ export default function () {
     }
   );
 
+  let baseUrl = $config.api.host;
+  // If we actually set the baseUrl to / then request URLs become absolute, e.g. https://api/v1/...
+  // This is not what we want, so we need to remove the leading slash.
+  if ($config.api.host === "/") {
+    baseUrl = undefined;
+  }
+
   // Create API instances
-  const adminApi = new AdminApi(undefined, $config.api.host, apiInstance);
-  const dashboardApi = new DashboardApi(undefined, $config.api.host, apiInstance);
-  const systemInfoApi = new SystemInfoApi(undefined, $config.api.host, apiInstance);
-  const codesApi = new CodesApi(undefined, $config.api.host, apiInstance);
-  const searchApi = new SearchApi(undefined, $config.api.host, apiInstance);
-  const facilitiesApi = new FacilitiesApi(undefined, $config.api.host, apiInstance);
-  const masterRecordsApi = new MasterRecordsApi(undefined, $config.api.host, apiInstance);
-  const patientRecordsApi = new PatientRecordsApi(undefined, $config.api.host, apiInstance);
-  const backgroundTasksApi = new BackgroundTasksApi(undefined, $config.api.host, apiInstance);
-  const patientIndexOperationsApi = new PatientIndexOperationsApi(undefined, $config.api.host, apiInstance);
-  const mirthApi = new MirthApi(undefined, $config.api.host, apiInstance);
-  const workItemsApi = new WorkItemsApi(undefined, $config.api.host, apiInstance);
-  const messagesApi = new MessagesApi(undefined, $config.api.host, apiInstance);
+  const adminApi = new AdminApi(undefined, baseUrl, apiInstance);
+  const dashboardApi = new DashboardApi(undefined, baseUrl, apiInstance);
+  const systemInfoApi = new SystemInfoApi(undefined, baseUrl, apiInstance);
+  const codesApi = new CodesApi(undefined, baseUrl, apiInstance);
+  const searchApi = new SearchApi(undefined, baseUrl, apiInstance);
+  const facilitiesApi = new FacilitiesApi(undefined, baseUrl, apiInstance);
+  const masterRecordsApi = new MasterRecordsApi(undefined, baseUrl, apiInstance);
+  const patientRecordsApi = new PatientRecordsApi(undefined, baseUrl, apiInstance);
+  const backgroundTasksApi = new BackgroundTasksApi(undefined, baseUrl, apiInstance);
+  const patientIndexOperationsApi = new PatientIndexOperationsApi(undefined, baseUrl, apiInstance);
+  const mirthApi = new MirthApi(undefined, baseUrl, apiInstance);
+  const workItemsApi = new WorkItemsApi(undefined, baseUrl, apiInstance);
+  const messagesApi = new MessagesApi(undefined, baseUrl, apiInstance);
 
   return {
     adminApi,
