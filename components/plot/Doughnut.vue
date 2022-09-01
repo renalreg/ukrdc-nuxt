441de1aac530fac1b80fd1516bee90250e861823
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-09-01 13:38:59 +0100

    Draft plotly doughnut component

---
+++ components/plot/Doughnut.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :id="id"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "@nuxtjs/composition-api";
+import Plotly, { Data } from "plotly.js-dist";
+import { plotColours } from "~/helpers/utils/colourUtils";
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Array,
+      default: null,
+    },
+    labels: {
+      type: Array,
+      default: null,
+    },
+    colors: {
+      type: Array,
+      default: () => {
+        return plotColours;
+      },
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+    legend: {
+      type: Boolean,
+      default: true,
+    },
+    legendLimit: {
+      type: Number,
+      default: 8,
+    },
+    id: {
+      type: String,
+      default: "doughnut",
+    },
+  },
+
+  setup(props) {
+    const data: Data[] = [
+      {
+        values: props.data as number[],
+        labels: props.labels as string[],
+        type: "pie",
+      },
+    ];
+
+    const layout = {
+      font: { size: 18 },
+    };
+
+    const config = { responsive: true };
+
+    onMounted(() => {
+      Plotly.newPlot(props.id, data, layout, config);
+    });
+  },
+});
+</script>
