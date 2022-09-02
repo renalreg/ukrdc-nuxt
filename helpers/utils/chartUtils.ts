3c310c4f51912033334ae23749a785e5c00f0cb0
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-09-02 16:16:26 +0100

    Add utility for unpacking history points

--- helpers/utils/chartUtils.ts
+++ helpers/utils/chartUtils.ts
@@ -1,7 +1,18 @@
+import { HistoryPoint } from "@ukkidney/ukrdc-axios-ts";
 import { DateTime } from "luxon";
 
 export function getPointDateRange(dateString: string) {
   const startDate = DateTime.fromISO(dateString);
   const endDate = startDate.plus({ days: 1 });
   return { since: startDate.toISO(), until: endDate.toISO() };
+}
+
+export function unpackHistoryPoints(historyPoints: HistoryPoint[]) {
+  const x: string[] = [];
+  const y: number[] = [];
+  historyPoints.forEach((point) => {
+    x.push(point.time);
+    y.push(point.count);
+  });
+  return { x, y };
 }
