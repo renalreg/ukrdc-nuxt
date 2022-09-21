ac622e18c2c7e0af4c09af9da769fe37d84bf3bb
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-09-21 16:01:30 +0100

    Fix survey viewer modal interface

--- interfaces/modal.ts
+++ interfaces/modal.ts
@@ -1,7 +1,15 @@
 import Vue from "vue";
+import { SurveySchema } from "@ukkidney/ukrdc-axios-ts";
 
 export interface modalInterface extends Vue {
   show: () => void;
+  hide: () => void;
+  toggle: () => void;
+  visible: boolean;
+}
+
+export interface surveyViewerModalInterface extends Vue {
+  show: (surveyToShow: SurveySchema) => void;
   hide: () => void;
   toggle: () => void;
   visible: boolean;
