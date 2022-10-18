549c73331f15f763614457ef8f5ae96a8b0b4cec
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-10-18 12:36:14 +0100

    Remove blackout scale transition

--- components/base/BaseBlackout.vue
+++ components/base/BaseBlackout.vue
@@ -5,7 +5,7 @@
     enter-to-class="opacity-100"
     leave-active-class="ease-in"
     leave-class="opacity-100"
-    leave-to-class="opacity-0 scale-90"
+    leave-to-class="opacity-0"
   >
     <div v-if="visible" class="fixed inset-0 transition-opacity" aria-hidden="true" @click="$emit('click')">
       <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
