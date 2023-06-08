a4c9042c5db8b806ecd1070ee86c11531459a012
Joel T. Collins <joel.collins@ukkidney.org>
2023-06-08 13:59:13 +0100

    Replace buggy treatments timeline with a data table

--- pages/patientrecords/_pid/medical/treatments.vue
+++ pages/patientrecords/_pid/medical/treatments.vue
@@ -3,115 +3,103 @@
     <BaseLoadingContainer :loading="!treatments">
       <p v-if="treatments && treatments.length <= 0" class="text-center">No treatments on record</p>
       <div v-else-if="treatments && treatments.length > 0" class="flow-root">
-        <ul role="list" class="-mb-8">
-          <li v-for="(treatment, index) in treatmentEvents" :key="index">
-            <div class="relative pb-8">
-              <span
-                v-if="index != treatmentEvents.length - 1"
-                class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200"
-                aria-hidden="true"
-              ></span>
-              <div class="relative flex space-x-3">
-                <div v-if="treatment.isDischarge">
-                  <span class="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 ring-8 ring-white">
-                    <!-- Heroicon name: solid/check -->
-                    <svg
-                      class="h-5 w-5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                </div>
-                <div v-else>
-                  <span class="flex h-8 w-8 items-center justify-center rounded-full bg-blue-500 ring-8 ring-white">
-                    <!-- Heroicon name: solid/login -->
-                    <svg
-                      class="h-5 w-5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
-                      />
-                    </svg>
-                  </span>
-                </div>
-                <div class="flex min-w-0 items-center gap-2">
-                  <div class="flex-shrink">
-                    <p class="mr-2 inline font-bold">
-                      <time :datetime="treatment.time">{{ treatment.time }}</time>
+        <!--        New UI-->
+        <BaseTable>
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-3">Facility Code</th>
+              <th scope="col" class="px-6 py-3">Admit Date</th>
+              <th scope="col" class="px-6 py-3">Admit Reason</th>
+              <th scope="col" class="px-6 py-3">Discharge Date</th>
+              <th scope="col" class="px-6 py-3">Discharge Reason</th>
+              <!-- Info tooltip -->
+              <th scope="col" class="px-6 py-3"></th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-300 bg-white">
+            <tr v-for="treatment in treatments" :key="treatment.id">
+              <td class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
+                <SendingFacilityLink class="font-medium" :code="treatment.healthCareFacilityCode" />
+              </td>
+              <!-- Admission-->
+              <td class="whitespace-nowrap px-6 py-4 text-gray-500">
+                {{ treatment.fromTime ? formatDate(treatment.fromTime, false) : "None" }}
+              </td>
+              <td class="truncate px-6 py-2 text-gray-500">
+                <CodeTitle
+                  v-if="treatment.admitReasonCodeStd && treatment.admitReasonCode"
+                  :coding-standard="treatment.admitReasonCodeStd"
+                  :code="treatment.admitReasonCode"
+                />
+                <p v-if="treatment.admitReasonDesc">{{ treatment.admitReasonDesc }}</p>
+              </td>
+              <!-- Discharge-->
+              <td class="whitespace-nowrap px-6 py-4 text-gray-500">
+                {{ treatment.toTime ? formatDate(treatment.toTime, false) : "None" }}
+              </td>
+              <td class="truncate px-6 py-2 text-gray-500">
+                <CodeTitle
+                  v-if="treatment.dischargeReasonCodeStd && treatment.dischargeReasonCode"
+                  :coding-standard="treatment.dischargeReasonCodeStd"
+                  :code="treatment.dischargeReasonCode"
+                />
+                <p v-if="treatment.dischargeReasonDesc">{{ treatment.dischargeReasonDesc }}</p>
+                <p
+                  v-if="
+                    !treatment.dischargeReasonDesc &&
+                    !(treatment.dischargeReasonCodeStd && treatment.dischargeReasonCode)
+                  "
+                >
+                  None
+                </p>
+              </td>
+              <!-- Info tooltip-->
+              <td>
+                <BaseInfoTooltip>
+                  <div class="sensitive">
+                    <p><b>ID: </b>{{ treatment.id }}</p>
+                    <br />
+                    <p><b>From time: </b>{{ treatment.fromTime }}</p>
+                    <p><b>To time: </b>{{ treatment.toTime || "None" }}</p>
+                    <br />
+                    <p><b>Admin reason: </b>{{ treatment.admitReasonDesc || "None" }}</p>
+                    <p>
+                      <b>Admin reason code: </b>{{ treatment.admitReasonCodeStd }} / {{ treatment.admitReasonCode }}
                     </p>
-                    <div v-if="treatment.isDischarge" class="inline">
-                      Discharged from
-                      {{ treatment.admitReasonDesc ? treatment.admitReasonDesc : treatment.admitReasonCode }} at
-                      <h5 class="inline">{{ treatment.healthCareFacilityCode }}</h5>
-                      {{
-                        treatment.dischargeReasonDesc
-                          ? `(${treatment.dischargeReasonDesc})`
-                          : treatment.dischargeReasonCode
-                          ? `(${treatment.dischargeReasonCode})`
-                          : ""
-                      }}
-                    </div>
-                    <div v-else class="inline">
-                      {{ treatment.time }} - Admitted to
-                      <h5 class="inline">{{ treatment.healthCareFacilityCode }}</h5>
-                      for
-                      {{ treatment.admitReasonDesc ? treatment.admitReasonDesc : treatment.admitReasonCode }}
-                    </div>
+                    <br />
+                    <p><b>Discharge reason: </b>{{ treatment.dischargeReasonDesc || "None" }}</p>
+                    <p>
+                      <b>Discharge reason code: </b>{{ treatment.dischargeReasonCodeStd || "None" }} /
+                      {{ treatment.dischargeReasonCode || "None" }}
+                    </p>
                   </div>
-                  <BaseInfoTooltip>
-                    <div class="sensitive">
-                      <p><b>From time: </b>{{ treatment.fromTime }}</p>
-                      <p><b>To time: </b>{{ treatment.toTime || "None" }}</p>
-                      <br />
-                      <p><b>Admin reason: </b>{{ treatment.admitReasonDesc || "None" }}</p>
-                      <p>
-                        <b>Admin reason code: </b>{{ treatment.admitReasonCodeStd }} / {{ treatment.admitReasonCode }}
-                      </p>
-                      <br />
-                      <p><b>Discharge reason: </b>{{ treatment.dischargeReasonDesc || "None" }}</p>
-                      <p>
-                        <b>Discharge reason code: </b>{{ treatment.dischargeReasonCodeStd || "None" }} /
-                        {{ treatment.dischargeReasonCode || "None" }}
-                      </p>
-                    </div>
-                  </BaseInfoTooltip>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
+                </BaseInfoTooltip>
+              </td>
+            </tr>
+          </tbody>
+        </BaseTable>
       </div>
     </BaseLoadingContainer>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, ref } from "@nuxtjs/composition-api";
+import { defineComponent, onMounted, ref } from "@nuxtjs/composition-api";
 import { PatientRecordSchema, TreatmentSchema } from "@ukkidney/ukrdc-axios-ts";
 
 import BaseInfoTooltip from "~/components/base/BaseInfoTooltip.vue";
 import BaseLoadingContainer from "~/components/base/BaseLoadingContainer.vue";
+import BaseTable from "~/components/base/BaseTable.vue";
+import CodeTitle from "~/components/CodeTitle.vue";
+import SendingFacilityLink from "~/components/SendingFacilityLink.vue";
 import useApi from "~/composables/useApi";
-
-interface TreatmentEvent extends TreatmentSchema {
-  time?: string;
-  isDischarge: boolean;
-}
+import { formatDate } from "~/helpers/dateUtils";
 
 export default defineComponent({
   components: {
+    CodeTitle,
+    SendingFacilityLink,
+    BaseTable,
     BaseLoadingContainer,
     BaseInfoTooltip,
   },
@@ -139,49 +127,9 @@ export default defineComponent({
         });
     });
 
-    // Sorted and paired treatment events
-    const treatmentEvents = computed(() => {
-      const events: TreatmentEvent[] = [];
-      for (const treatment of treatments.value || []) {
-        if (treatment.toTime) {
-          const event = {
-            time: treatment.toTime,
-            isDischarge: true,
-            ...treatment,
-          };
-          events.push(event);
-        }
-        const event = {
-          time: treatment.fromTime,
-          isDischarge: false,
-          ...treatment,
-        };
-        events.push(event);
-      }
-      events.sort(function (a, b) {
-        if (a.time === undefined || b.time === undefined) {
-          return 0;
-        }
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
-        if (a.time === b.time) {
-          // If they're part of the same treatment
-          if (a.admitReasonDesc === b.admitReasonDesc) {
-            // Admit before discharge
-            return (b.isDischarge ? 1 : 0) - (a.isDischarge ? 1 : 0);
-          } else {
-            // Discharge before admit
-            return (a.isDischarge ? 1 : 0) - (b.isDischarge ? 1 : 0);
-          }
-        }
-        return new Date(b.time).getTime() - new Date(a.time).getTime();
-      });
-      return events;
-    });
-
     return {
+      formatDate,
       treatments,
-      treatmentEvents,
     };
   },
 });
