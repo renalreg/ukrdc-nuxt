88e3340ed2aa9df7d95bd7794e33642a1d65efc1
Joel T. Collins <joel.collins@ukkidney.org>
2023-06-08 13:57:35 +0100

    Improve base table layout

--- components/base/BaseTable.vue
+++ components/base/BaseTable.vue
@@ -2,7 +2,7 @@
   <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 md:-mx-8">
     <div class="inline-block min-w-full overflow-hidden px-4 py-2 align-middle sm:px-6 md:px-8">
       <BaseCard>
-        <table class="min-w-full divide-y divide-gray-300">
+        <table class="min-w-full table-auto divide-y divide-gray-300">
           <slot></slot>
         </table>
       </BaseCard>
