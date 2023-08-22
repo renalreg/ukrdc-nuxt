fdaf9a06c62a16e3bc02e51ac39f7b717cf05de7
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-08-22 10:35:25 +0100

    Clarify the meaning of missing filenames in the messages list

--- components/messages/MessagesListItem.vue
+++ components/messages/MessagesListItem.vue
@@ -3,14 +3,20 @@
     <div class="group grid w-full min-w-0 grid-cols-2 gap-2 px-4 py-4 sm:grid-cols-3 sm:px-6 lg:grid-cols-5">
       <!-- Heading -->
       <div class="col-span-5 lg:col-span-3">
-        <div class="truncate">
-          <h5 class="sensitive truncate md:inline">
-            {{ item.filename || "No filename found" }}
-          </h5>
-          <h5 class="truncate md:inline">
-            {{ item.mirthChannel ? `on ${item.mirthChannel}` : "" }}
-          </h5>
+        <div class="flex gap-2">
+          <div class="truncate">
+            <h5 class="sensitive truncate md:inline">
+              {{ item.filename || "Internal file" }}
+            </h5>
+            <h5 class="truncate md:inline">
+              {{ item.mirthChannel ? `on ${item.mirthChannel}` : "" }}
+            </h5>
+          </div>
+          <BaseInfoTooltip v-if="!item.filename" class="inline">
+            <p>This usually corresponds to a file internally sent to update RADAR membership details.</p>
+          </BaseInfoTooltip>
         </div>
+
         <div class="mt-2 flex">
           <BadgeMessageStatus class="mr-2 flex-shrink" :message="item" />
           <p class="line-clamp-1 flex-grow">
@@ -55,6 +61,7 @@ import { MessageSchema } from "@ukkidney/ukrdc-axios-ts";
 
 import BadgeMessageStatus from "~/components/BadgeMessageStatus.vue";
 import BaseButton from "~/components/base/BaseButton.vue";
+import BaseInfoTooltip from "~/components/base/BaseInfoTooltip.vue";
 import IconFunnel from "~/components/icons/hero/20/solid/IconFunnel.vue";
 import SendingFacilityLink from "~/components/SendingFacilityLink.vue";
 import useSensitive from "~/composables/useSensitive";
@@ -63,6 +70,7 @@ import { makeMessageSummary } from "~/helpers/messageUtils";
 
 export default defineComponent({
   components: {
+    BaseInfoTooltip,
     BaseButton,
     SendingFacilityLink,
     IconFunnel,
